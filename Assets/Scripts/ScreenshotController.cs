8bcba6669e560e63a99bfcbad83a10e32b6a41ba
Trent M <trenthm@gmail.com>
2019-03-27 14:57:59 -0500

    Initial commit

---
+++ Assets/Scripts/ScreenshotController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+
+public class ScreenshotController : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            Debug.Log("Screenshots/" + DateTime.Now.ToString("MM-dd-yyyy HH.mm.ss") + ".png");
+            ScreenCapture.CaptureScreenshot("Screenshots/" + DateTime.Now.ToString("MM-dd-yyyy HH.mm.ss") + ".png");
+        }
+    }
+}
